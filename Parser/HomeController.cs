057f620ff66eecb69c7d404d38f74a977455c81c
SpecOp0 <SpecOp0@yahoo.de>
2022-02-19 19:52:24 +0100

    refactor: split logic to get news and present relevant one into different methods

--- Parser/HomeController.cs
+++ Parser/HomeController.cs
@@ -13,18 +13,55 @@ namespace Parser
         [Route("/")]
         public async Task<IActionResult> Index()
         {
-            var relevantEntries = await this.ParseEntries();
+            var entries = await this.GetNews();
+
+            // return new entries
+            var relevantEntries = new List<NewsEntry?>(entries.New);
+            relevantEntries.Add(null);
+
+            // return some old entries
+            var oldEntriesCount = entries.New.Count > 10 ? 70 : 140;
+            relevantEntries.AddRange(entries.Old.Take(oldEntriesCount));
 
             return this.Content(relevantEntries.ToHtml(), "text/html;charset=utf-8");
         }
 
-        private async Task<ICollection<NewsEntry?>> ParseEntries()
+        private async Task<(ICollection<NewsEntry> New, ICollection<NewsEntry> Old)> GetNews()
         {
             var database = this.HttpContext.RequestServices.GetRequiredService<DatabaseService>();
             var browser = this.HttpContext.RequestServices.GetRequiredService<Browser>();
 
-            var oldEntries = new HashSet<NewsEntry>(await database.GetEntries());
-            var entriesToSave = new List<NewsEntry>(oldEntries);
+            var oldEntries = (await database.GetEntries()).ToList();
+            var oldEntriesSet = new HashSet<NewsEntry>(oldEntries);
+
+            var newEntries = new List<NewsEntry>();
+
+            foreach (var newEntry in this.GetLatestNews().Reverse())
+            {
+                if (oldEntriesSet.TryGetValue(newEntry, out var oldEntry))
+                {
+                    // reorder old entry (has image and should not be removed)
+                    oldEntries.Remove(oldEntry);
+                    oldEntries.Insert(0, oldEntry);
+                }
+                else
+                {
+                    // load image and set it as base64 encoded image
+                    newEntry.GetAndSetImage(browser);
+
+                    newEntries.Add(newEntry);
+                }
+            }
+
+            // save at most 400 entries
+            await database.SetEntries(newEntries.Concat(oldEntries).Take(400).ToArray());
+
+            return (newEntries, oldEntries);
+        }
+
+        private ICollection<NewsEntry> GetLatestNews()
+        {
+            var browser = this.HttpContext.RequestServices.GetRequiredService<Browser>();
 
             var allParsedEntries = new List<NewsEntry>();
             var plugins = Plugins.Plugins.GetPlugins();
@@ -40,51 +77,7 @@ namespace Parser
                 }
             }
 
-            var entriesToReturn = new List<NewsEntry?>();
-            var isFirstNewEntry = true;
-            foreach (var newEntry in allParsedEntries.AsEnumerable().Reverse())
-            {
-                if (oldEntries.TryGetValue(newEntry, out var oldEntry))
-                {
-                    // reorder old entry (has image and should not be removed)
-                    entriesToSave.Remove(oldEntry);
-                    entriesToSave.Insert(0, oldEntry);
-                    continue;
-                }
-
-                // load image and set it as base64 encoded image
-                newEntry.GetAndSetImage(browser);
-
-                // return new entry
-                if (isFirstNewEntry)
-                {
-                    entriesToReturn.Insert(0, null);
-                    isFirstNewEntry = false;
-                }
-                entriesToReturn.Insert(0, newEntry);
-
-                // save new entry
-                oldEntries.Add(newEntry);
-                entriesToSave.Insert(0, newEntry);
-            }
-
-            // no new entry found
-            if (isFirstNewEntry)
-            {
-                entriesToReturn.Insert(0, null);
-            }
-
-            // return some old entries
-            entriesToReturn.AddRange(
-                entriesToSave
-                    // skip already present entries (has 1 additional <null> entry)
-                    .Skip(entriesToReturn.Count - 1)
-                    .Take(entriesToReturn.Count > 10 ? 70 : 140));
-
-            // save at most 400 entries
-            await database.SetEntries(entriesToSave.Take(400).ToArray());
-
-            return entriesToReturn;
+            return allParsedEntries;
         }
     }
 }
