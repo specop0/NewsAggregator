20d0fb6aa47ed46e5e3a85020486dd56f93e85d2
SpecOp0 <SpecOp0@yahoo.de>
2025-06-09 18:31:56 +0200

    fix: allocated memory not freed (2)

--- NewsAggregator/Database/DatabaseService.cs
+++ NewsAggregator/Database/DatabaseService.cs
@@ -1,16 +1,22 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 
 namespace NewsAggregator.Database;
 
-public class DatabaseService : RestServiceBase
+public class DatabaseService
 {
-    public DatabaseService(IConfiguration configuration) : base(GetBaseUrl(configuration))
+    public DatabaseService(IHttpClientFactory clientFactory, IConfiguration configuration)
     {
+        this.Client = clientFactory.CreateClient(nameof(DatabaseService));
+        this.Client.BaseAddress = new Uri(GetBaseUrl(configuration));
     }
+
+    private HttpClient Client { get; }
 
     private const string NewsKey = "News";
 
@@ -35,5 +41,26 @@ public class DatabaseService : RestServiceBase
     {
         var serializedEntries = JsonSerializer.Serialize(new RestData<NewsEntry[]> { Data = entries.ToArray() });
         await this.Do(NewsKey, "PUT", serializedEntries);
+    }
+
+    protected async Task<string?> Do(string url, string httpMethod, string? input = null)
+    {
+        var request = new HttpRequestMessage(new HttpMethod(httpMethod), url);
+        if (input != null)
+        {
+            request.Content = new StringContent(input);
+        }
+
+        var response = await this.Client.SendAsync(request);
+
+        response.EnsureSuccessStatusCode();
+
+        if (response.Content != null)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            return content;
+        }
+
+        return null;
     }
 }
